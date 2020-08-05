5335a612499795fba4403f2d21f83e6538540adf
masb0ymas <fajriajjha@gmail.com>
2020-08-05 22:59:31 +0700

    add: seeder role

---
+++ src/seeders/20200805153803-Seeder-Role.js
@@ -0,0 +1,29 @@
+import ConstRole from '../constants/ConstRole'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Roles', [
+      {
+        id: ConstRole.ID_ADMIN,
+        nama: 'Admin',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        id: ConstRole.ID_UMUM,
+        nama: 'Umum',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+}
