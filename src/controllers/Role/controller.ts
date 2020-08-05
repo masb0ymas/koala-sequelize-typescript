7521e60b6c684ebf8a6e8597816730777fe94b20
masb0ymas <fajriajjha@gmail.com>
2020-08-05 23:19:21 +0700

    fix: crud role controller

--- src/controllers/Role/controller.ts
+++ src/controllers/Role/controller.ts
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
-import { BaseContext, BaseRequest, Context } from 'koa'
+import { Context } from 'koa'
 import models from '../../models'
 
 const { Roles } = models
 
 export default class RoleController {
-  public static async getAll(ctx: BaseContext) {
+  public static async getAll(ctx: Context) {
     const data = await Roles.findAll()
     const total = await Roles.count()
 
@@ -17,11 +17,71 @@ export default class RoleController {
   }
 
   public static async getOne(ctx: Context) {
-    const data = await Roles.findByPk(ctx.params.id)
+    const { id } = ctx.params
+    const data = await Roles.findByPk(id)
 
-    ctx.status = 200
+    if (!data) {
+      ctx.status = 404
+      ctx.body = {
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+      }
+    } else {
+      ctx.status = 200
+      ctx.body = {
+        data,
+      }
+    }
+  }
+
+  public static async create(ctx: Context) {
+    const data = await Roles.create(ctx.request.body)
+
+    ctx.status = 201
     ctx.body = {
+      message: 'Data berhasil ditambahkan',
       data,
+    }
+  }
+
+  public static async update(ctx: Context) {
+    const { id } = ctx.params
+    const data = await Roles.findByPk(id)
+
+    if (!data) {
+      ctx.status = 404
+      ctx.body = {
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+        data,
+      }
+    } else {
+      await data.update(ctx.request.body || {})
+
+      ctx.status = 200
+      ctx.body = {
+        message: 'Data berhasil diperbarui!',
+        data,
+      }
+    }
+  }
+
+  public static async delete(ctx: Context) {
+    const { id } = ctx.params
+    const data = await Roles.findByPk(id)
+
+    if (!data) {
+      ctx.status = 404
+      ctx.body = {
+        message: 'Data tidak ditemukan atau sudah terhapus!',
+        data,
+      }
+    } else {
+      await data?.destroy()
+
+      ctx.status = 200
+      ctx.body = {
+        message: 'Data berhasil dihapus!',
+        data,
+      }
     }
   }
 }
