f1c38bc43085c64588fb6392fb338a434064da12
masb0ymas <fajriajjha@gmail.com>
2020-08-05 23:00:16 +0700

    add: role controller

--- src/controllers/Role/controller.ts
+++ src/controllers/Role/controller.ts
@@ -1,20 +1,27 @@
 /* eslint-disable no-unused-vars */
-import { BaseContext } from 'koa'
+import { BaseContext, BaseRequest, Context } from 'koa'
+import models from '../../models'
+
+const { Roles } = models
 
 export default class RoleController {
   public static async getAll(ctx: BaseContext) {
+    const data = await Roles.findAll()
+    const total = await Roles.count()
+
     ctx.status = 200
     ctx.body = {
-      data: [
-        {
-          id: 1,
-          nama: 'Umum',
-        },
-        {
-          id: 2,
-          nama: 'Test',
-        },
-      ],
+      data,
+      total,
+    }
+  }
+
+  public static async getOne(ctx: Context) {
+    const data = await Roles.findByPk(ctx.params.id)
+
+    ctx.status = 200
+    ctx.body = {
+      data,
     }
   }
 }
