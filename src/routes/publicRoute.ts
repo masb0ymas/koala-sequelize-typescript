26aeb0ca730a9195bdfcf5f0bc6806953ec4494c
masb0ymas <fajriajjha@gmail.com>
2020-08-05 23:19:11 +0700

    add: method role

--- src/routes/publicRoute.ts
+++ src/routes/publicRoute.ts
@@ -6,6 +6,9 @@ import RoleController from '../controllers/Role/controller'
 const router = new Router()
 
 router.get('/role', RoleController.getAll)
+router.post('/role', RoleController.create)
 router.get('/role/:id', RoleController.getOne)
+router.put('/role/:id', RoleController.update)
+router.delete('/role/:id', RoleController.delete)
 
 export default router
