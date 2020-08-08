1cc55e3089e9cc117cd883f98754ac1f7bd8ccfd
masb0ymas <fajriajjha@gmail.com>
2020-08-08 21:33:37 +0700

    add: route auth

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
 import Router from 'koa-router'
+import AuthController from '../controllers/Auth/controller'
 import RoleController from '../controllers/Role/controller'
 
 const router = new Router()
+
+router.post('/auth/sign-in', AuthController.signIn)
+router.post('/auth/sign-up', AuthController.signUp)
 
 router.get('/role', RoleController.getAll)
 router.post('/role', RoleController.create)
