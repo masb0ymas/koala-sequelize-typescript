1b3f72cfdc9bebad26ca062d7b5489075679b5ac
masb0ymas <fajriajjha@gmail.com>
2020-08-08 21:36:29 +0700

    add: private route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@
 import { BaseContext } from 'koa'
 import Router from 'koa-router'
 import publicRoute from './public'
+import privateRoute from './private'
 
 const router = new Router()
 
@@ -19,7 +20,11 @@ router.get('/v1', (ctx: BaseContext) => {
   }
 })
 
+// Public Routes
 router.use('/v1', publicRoute.routes())
 router.use('/v1', publicRoute.allowedMethods())
+// Private Routes
+router.use('/v1', privateRoute.routes())
+router.use('/v1', privateRoute.allowedMethods())
 
 export default router
