78b54cd1a80debc7c4ad07e72cd0d753e4d116fb
masb0ymas <fajriajjha@gmail.com>
2020-08-16 19:39:45 +0700

    fix: import module in routes private

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import Router from 'koa-router'
-import AuthController from '../controllers/Auth/controller'
+import AuthController from 'controllers/Auth/controller'
 
 const router = new Router()
 
