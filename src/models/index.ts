5cdec6c66d90af998bc4d20db56fbf514385c660
masb0ymas <fajriajjha@gmail.com>
2020-08-21 22:37:45 +0700

    add: interface filter query

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,23 @@
 import Role from './roles'
 import User from './user'
 
+export interface FilterAttributes {
+  id: string
+  value: string
+}
+
+export interface SortAttributes {
+  id: string
+  desc: string
+}
+
+export interface FilterQueryAttributes {
+  page: string | number
+  pageSize: string | number
+  filtered: string
+  sorted: string
+}
+
 export default {
   Role,
   User,
