cd96a30765c5ddb61cd0b0f3f010830743463e46
masb0ymas <fajriajjha@gmail.com>
2020-08-21 22:38:28 +0700

    fix: routes public

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -1,17 +1,9 @@
 /* eslint-disable no-unused-vars */
 import Router from 'koa-router'
-import AuthController from 'controllers/Auth/controller'
-import RoleController from 'controllers/Role/controller'
 
 const router = new Router()
 
-router.post('/auth/sign-in', AuthController.signIn)
-router.post('/auth/sign-up', AuthController.signUp)
-
-router.get('/role', RoleController.getAll)
-router.post('/role', RoleController.create)
-router.get('/role/:id', RoleController.getOne)
-router.put('/role/:id', RoleController.update)
-router.delete('/role/:id', RoleController.delete)
-
 export default router
+
+require('controllers/Auth/controller')
+require('controllers/Role/controller')
