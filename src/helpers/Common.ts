5b84f4cdec94589b44e6104204b49d4fd2ad6085
masb0ymas <fajriajjha@gmail.com>
2020-08-21 22:37:34 +0700

    add: filter query object

--- src/helpers/Common.ts
+++ src/helpers/Common.ts
@@ -1,9 +1,15 @@
+/* eslint-disable no-unused-vars */
 import fs from 'fs'
 import jwt, {
   JsonWebTokenError,
   TokenExpiredError,
   NotBeforeError,
 } from 'jsonwebtoken'
+import db from 'models/_instance'
+import { FilterAttributes } from 'models'
+
+const { Sequelize } = db
+const { Op } = Sequelize
 
 const { JWT_SECRET }: any = process.env
 
@@ -17,6 +23,26 @@ function getUniqueCodev2(length = 32) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength))
   }
   return result
+}
+
+// Filter Query Object
+function filterQueryObject(filtered: FilterAttributes[]) {
+  const resultObject = {}
+  if (typeof filtered !== 'object') {
+    throw new Error(`Filtered must be an object, expected ${typeof filtered}`)
+  }
+
+  for (let i = 0; i < filtered.length; i += 1) {
+    // eslint-disable-next-line prefer-const
+    let { id, value } = filtered[i]
+    if (id.split('.').length > 1) {
+      id = `$${id}$`
+    }
+    // @ts-ignore
+    resultObject[id] = { [Op.like]: `%${value}%` }
+  }
+
+  return resultObject
 }
 
 // Get Token from headers
@@ -68,4 +94,10 @@ function readHTMLFile(path: any, callback: any) {
   })
 }
 
-export { getUniqueCodev2, getToken, verifyToken, readHTMLFile }
+export {
+  getUniqueCodev2,
+  filterQueryObject,
+  getToken,
+  verifyToken,
+  readHTMLFile,
+}
