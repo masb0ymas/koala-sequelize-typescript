41f3e066a38e5f41ca2202cffbaa37a1df55ea39
masb0ymas <fajriajjha@gmail.com>
2020-08-21 22:38:20 +0700

    fix: route private

--- src/routes/private.ts
+++ src/routes/private.ts
@@ -1,9 +1,6 @@
 /* eslint-disable no-unused-vars */
 import Router from 'koa-router'
-import AuthController from 'controllers/Auth/controller'
 
 const router = new Router()
-
-router.get('/auth/profile', AuthController.profile)
 
 export default router
