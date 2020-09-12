37dc7db17dead51b9479c65475022617ace3261e
masb0ymas <fajriajjha@gmail.com>
2020-09-12 21:07:28 +0700

    fix: remove jwt token

--- src/helpers/Common.ts
+++ src/helpers/Common.ts
@@ -1,17 +1,11 @@
 /* eslint-disable no-unused-vars */
 import fs from 'fs'
-import jwt, {
-  JsonWebTokenError,
-  TokenExpiredError,
-  NotBeforeError,
-} from 'jsonwebtoken'
+
 import db from 'models/_instance'
 import { FilterAttributes } from 'models'
 
 const { Sequelize } = db
 const { Op } = Sequelize
-
-const { JWT_SECRET }: any = process.env
 
 // Generate Unique Code ( default length 32 )
 function getUniqueCodev2(length = 32) {
@@ -45,44 +39,6 @@ function filterQueryObject(filtered: FilterAttributes[]) {
   return resultObject
 }
 
-// Get Token from headers
-function getToken(headers: any) {
-  if (headers && headers.authorization) {
-    const parted = headers.authorization.split(' ')
-    if (parted.length === 2) {
-      return parted[1]
-    }
-    return null
-  }
-  return null
-}
-
-// Verify Token
-function verifyToken(header: any) {
-  const token = getToken(header)
-
-  try {
-    if (!token) {
-      return { data: null, message: 'Token not found!' }
-    }
-
-    const data = jwt.verify(token, JWT_SECRET)
-    return { data, message: 'Token is verify' }
-  } catch (err) {
-    if (err instanceof TokenExpiredError) {
-      return { data: null, message: `Token ${err.message}` }
-    }
-
-    if (err instanceof JsonWebTokenError) {
-      return { data: null, message: `Token ${err.message}` }
-    }
-
-    if (err instanceof NotBeforeError) {
-      return { data: null, message: `Token ${err.message}` }
-    }
-  }
-}
-
 // Read HTML File
 function readHTMLFile(path: any, callback: any) {
   fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
@@ -94,10 +50,4 @@ function readHTMLFile(path: any, callback: any) {
   })
 }
 
-export {
-  getUniqueCodev2,
-  filterQueryObject,
-  getToken,
-  verifyToken,
-  readHTMLFile,
-}
+export { getUniqueCodev2, filterQueryObject, readHTMLFile }
