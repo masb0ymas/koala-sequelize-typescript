d17c5e62d8649ede72fee63b55d0d50bc217bae7
masb0ymas <fajriajjha@gmail.com>
2020-09-12 21:07:52 +0700

    feat: auth middleware

---
+++ src/middlewares/Authorization.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import { Context, Next } from 'koa'
+import { verifyToken } from 'helpers/Token'
+import { isEmpty } from 'lodash'
+
+async function Authorization(ctx: Context, next: Next) {
+  const token = verifyToken(ctx.request.header)
+
+  if (isEmpty(token?.data)) {
+    ctx.status = 401
+    ctx.body = {
+      message: token?.message,
+    }
+    return
+  }
+
+  await next()
+}
+
+export default Authorization
