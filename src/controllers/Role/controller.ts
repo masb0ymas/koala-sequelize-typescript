ee40680b62bcd81ba28c1622de83cc2a5b309a39
masb0ymas <fajriajjha@gmail.com>
2020-11-07 17:22:18 +0700

    refactor: role controller

--- src/controllers/Role/controller.ts
+++ src/controllers/Role/controller.ts
@@ -1,71 +1,54 @@
-/* eslint-disable no-unused-vars */
 import { Context } from 'koa'
-import { FilterQueryAttributes } from 'models'
 import routes from 'routes/public'
 import Authorization from 'middlewares/Authorization'
+import BuildResponse from 'modules/Response/BuildResponse'
 import RoleService from './service'
 
 routes.get('/role', async (ctx: Context) => {
-  const {
-    page,
-    pageSize,
-    filtered,
-    sorted,
-  }: FilterQueryAttributes = ctx.request.query
-  const { data, total } = await RoleService.getAll(
-    page,
-    pageSize,
-    filtered,
-    sorted
-  )
+  const { message, data, total } = await RoleService.getAll(ctx)
+  const buildResponse = BuildResponse.get({ message, data, total })
 
   ctx.status = 200
-  ctx.body = {
-    data,
-    total,
-  }
+  ctx.body = buildResponse
 })
 
 routes.get('/role/:id', async (ctx: Context) => {
   const { id } = ctx.params
-  const { status, message, data } = await RoleService.getOne(id)
 
-  ctx.status = status
-  ctx.body = {
-    message,
-    data,
-  }
+  const { code, message, data } = await RoleService.getOne(id)
+  const buildResponse = BuildResponse.get({ message, data })
+
+  ctx.status = code
+  ctx.body = buildResponse
 })
 
 routes.post('/role', Authorization, async (ctx: Context) => {
   const formData = ctx.request.body
-  const { message, data } = await RoleService.create(formData)
+
+  const data = await RoleService.create(formData)
+  const buildResponse = BuildResponse.created({ data })
 
   ctx.status = 201
-  ctx.body = {
-    message,
-    data,
-  }
+  ctx.body = buildResponse
 })
 
 routes.put('/role/:id', Authorization, async (ctx: Context) => {
   const { id } = ctx.params
   const formData = ctx.request.body
-  const { status, message, data } = await RoleService.update(id, formData)
 
-  ctx.status = status
-  ctx.body = {
-    message,
-    data,
-  }
+  const data = await RoleService.update(id, formData)
+  const buildResponse = BuildResponse.updated({ data })
+
+  ctx.status = 200
+  ctx.body = buildResponse
 })
 
 routes.delete('/role/:id', Authorization, async (ctx: Context) => {
   const { id } = ctx.params
-  const { status, message } = await RoleService.delete(id)
 
-  ctx.status = status
-  ctx.body = {
-    message,
-  }
+  await RoleService.delete(id)
+  const buildResponse = BuildResponse.deleted({})
+
+  ctx.status = 200
+  ctx.body = buildResponse
 })
