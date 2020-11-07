c36cba6fe09d8b78f424737aecff45693e57a0d4
masb0ymas <fajriajjha@gmail.com>
2020-11-07 17:21:31 +0700

    refactor: auth controller

--- src/controllers/Auth/controller.ts
+++ src/controllers/Auth/controller.ts
@@ -2,38 +2,35 @@
 import { Context } from 'koa'
 import routes from 'routes/public'
 import { verifyToken } from 'helpers/Token'
+import BuildResponse from 'modules/Response/BuildResponse'
 import AuthService from './service'
 
 routes.post('/auth/sign-up', async (ctx: Context) => {
   const formData = ctx.request.body
+
   const { message, data } = await AuthService.signUp(formData)
+  const buildResponse = BuildResponse.get({ message, data })
 
   ctx.status = 200
-  ctx.body = {
-    message,
-    data,
-  }
+  ctx.body = buildResponse
 })
 
 routes.post('/auth/sign-in', async (ctx: Context) => {
   const formData = ctx.request.body
-  const { status, message, data } = await AuthService.signIn(formData)
 
-  ctx.status = status
-  ctx.body = {
-    message,
-    data,
-  }
+  const { code, message, data } = await AuthService.signIn(formData)
+  const buildResponse = BuildResponse.get({ code, message, data })
+
+  ctx.status = code
+  ctx.body = buildResponse
 })
 
 routes.get('/auth/profile', async (ctx: Context) => {
   const token = verifyToken(ctx.request.header)
   // @ts-ignore
-  const { status, message, data } = await AuthService.profile(token)
+  const { code, message, data } = await AuthService.profile(token)
+  const buildResponse = BuildResponse.get({ code, message, data })
 
-  ctx.status = status
-  ctx.body = {
-    message,
-    data,
-  }
+  ctx.status = code
+  ctx.body = buildResponse
 })
