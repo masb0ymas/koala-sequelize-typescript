8effc236f6c122116faf80d0261446b56a593c55
masb0ymas <fajriajjha@gmail.com>
2020-11-07 17:22:06 +0700

    refactor: role service

--- src/controllers/Role/service.ts
+++ src/controllers/Role/service.ts
@@ -1,9 +1,7 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-param-reassign */
 import models from 'models'
-import { filterQueryObject } from 'helpers/Common'
 import useValidation from 'helpers/useValidation'
 import { RoleAttributes } from 'models/roles'
+import PluginSqlizeQuery from 'modules/SqlizeQuery/PluginSqlizeQuery'
 import schema from './schema'
 
 const { Role } = models
@@ -12,28 +10,23 @@ class RoleService {
   /**
    * Get All Role
    */
-  public static async getAll(
-    page: string | number,
-    pageSize: string | number,
-    filtered: string,
-    sorted: string
-  ) {
-    if (!page) page = 0
-    if (!pageSize) pageSize = 10
-
-    const filterObject = filtered ? filterQueryObject(JSON.parse(filtered)) : []
+  public static async getAll(ctx: any) {
+    const { includeCount, order, ...queryFind } = PluginSqlizeQuery.generate(
+      ctx,
+      Role,
+      []
+    )
 
     const data = await Role.findAll({
-      where: filterObject,
-      offset: Number(pageSize) * Number(page),
-      limit: Number(pageSize),
-      order: [['createdAt', 'desc']],
+      ...queryFind,
+      order: order.length ? order : [['createdAt', 'desc']],
     })
     const total = await Role.count({
-      where: filterObject,
+      include: includeCount,
+      where: queryFind.where,
     })
 
-    return { data, total }
+    return { message: `${total} data has been received.`, data, total }
   }
 
   /**
@@ -42,17 +35,15 @@ class RoleService {
   public static async getOne(id: string) {
     const data = await Role.findByPk(id)
 
-    let message = ''
-    let status = 0
-    if (data) {
-      status = 200
-      message = 'The data was successfully obtained!'
-    } else {
-      status = 404
+    let code = 200
+    let message = 'data has been received'
+
+    if (!data) {
+      code = 404
       message = 'Data not found or has been deleted!'
     }
 
-    return { status, message, data }
+    return { code, message, data }
   }
 
   /**
@@ -62,14 +53,14 @@ class RoleService {
     const value = useValidation(schema.create, formData)
     const data = await Role.create(value)
 
-    return { message: 'Data has been added!', data }
+    return data
   }
 
   /**
    * Update Role By Id
    */
   public static async update(id: string, formData: RoleAttributes) {
-    const { status, data } = await this.getOne(id)
+    const { data } = await this.getOne(id)
 
     if (data) {
       const value = useValidation(schema.create, {
@@ -80,20 +71,18 @@ class RoleService {
       await data.update(value || {})
     }
 
-    return { status, message: 'Data updated successfully!', data }
+    return data
   }
 
   /**
    * Delete Role By Id
    */
   public static async delete(id: string) {
-    const { status, data } = await this.getOne(id)
+    const { data } = await this.getOne(id)
 
     if (data) {
       await data.destroy()
     }
-
-    return { status, message: 'Data deleted successfully!' }
   }
 }
 
