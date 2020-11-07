c6512b2187266313592ed25b4460f868e0bc2f1e
masb0ymas <fajriajjha@gmail.com>
2020-11-07 21:17:27 +0700

    fix: role controller

--- src/controllers/Role/controller.ts
+++ src/controllers/Role/controller.ts
@@ -16,7 +16,7 @@ routes.get('/role/:id', async (ctx: Context) => {
   const { id } = ctx.params
 
   const { code, message, data } = await RoleService.getOne(id)
-  const buildResponse = BuildResponse.get({ message, data })
+  const buildResponse = BuildResponse.get({ code, message, data })
 
   ctx.status = code
   ctx.body = buildResponse
@@ -36,8 +36,8 @@ routes.put('/role/:id', Authorization, async (ctx: Context) => {
   const { id } = ctx.params
   const formData = ctx.request.body
 
-  const data = await RoleService.update(id, formData)
-  const buildResponse = BuildResponse.updated({ data })
+  const { code, message, data } = await RoleService.update(id, formData)
+  const buildResponse = BuildResponse.updated({ code, message, data })
 
   ctx.status = 200
   ctx.body = buildResponse
@@ -46,8 +46,8 @@ routes.put('/role/:id', Authorization, async (ctx: Context) => {
 routes.delete('/role/:id', Authorization, async (ctx: Context) => {
   const { id } = ctx.params
 
-  await RoleService.delete(id)
-  const buildResponse = BuildResponse.deleted({})
+  const { code, message } = await RoleService.delete(id)
+  const buildResponse = BuildResponse.deleted({ code, message })
 
   ctx.status = 200
   ctx.body = buildResponse
