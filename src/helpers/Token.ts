551d0353d9805b911679421201c592a169fca664
masb0ymas <fajriajjha@gmail.com>
2021-01-06 18:58:40 +0700

    fix: update token helper

--- src/helpers/Token.ts
+++ src/helpers/Token.ts
@@ -6,30 +6,39 @@ import jwt, {
 
 require('dotenv').config()
 
-const { JWT_SECRET }: any = process.env
+const { JWT_SECRET_ACCESS_TOKEN, JWT_SECRET_REFRESH_TOKEN }: any = process.env
 
-// Get Token from headers
+/**
+ *
+ * @param headers
+ */
 function getToken(headers: any) {
   if (headers && headers.authorization) {
     const parted = headers.authorization.split(' ')
-    if (parted.length === 2) {
-      return parted[1]
+
+    // Check Bearer xxx || JWT xxx
+    if (parted[0] === 'Bearer' || parted[0] === 'JWT') {
+      if (parted.length === 2) {
+        return parted[1]
+      }
     }
+
     return null
   }
   return null
 }
 
-// Verify Token
-function verifyToken(header: any) {
-  const token = getToken(header)
-
+/**
+ *
+ * @param token - Verify Token
+ */
+function verifyAccessToken(token: string) {
   try {
     if (!token) {
       return { data: null, message: 'Unauthorized!' }
     }
 
-    const data = jwt.verify(token, JWT_SECRET)
+    const data = jwt.verify(token, JWT_SECRET_ACCESS_TOKEN)
     return { data, message: 'Token is verify' }
   } catch (err) {
     if (err instanceof TokenExpiredError) {
@@ -46,4 +55,31 @@ function verifyToken(header: any) {
   }
 }
 
-export { getToken, verifyToken }
+/**
+ *
+ * @param token - Verify Token
+ */
+function verifyRefreshToken(token: string) {
+  try {
+    if (!token) {
+      return { data: null, message: 'Unauthorized!' }
+    }
+
+    const data = jwt.verify(token, JWT_SECRET_REFRESH_TOKEN)
+    return { data, message: 'Token is verify' }
+  } catch (err) {
+    if (err instanceof TokenExpiredError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+
+    if (err instanceof JsonWebTokenError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+
+    if (err instanceof NotBeforeError) {
+      return { data: null, message: `Token ${err.message}` }
+    }
+  }
+}
+
+export { getToken, verifyAccessToken, verifyRefreshToken }
