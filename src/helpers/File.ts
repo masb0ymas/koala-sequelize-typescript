e813d0ca1f636d56f2eeae23c486ce5c2a886a36
masb0ymas <fajriajjha@gmail.com>
2021-03-24 07:40:21 +0700

    feat(helper): add file helper

---
+++ src/helpers/File.ts
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import path from 'path'
+
+/**
+ *
+ * @param path - path file template html
+ * @param callback
+ */
+function readHTMLFile(path: any, callback: any) {
+  fs.readFile(path, { encoding: 'utf-8' }, function (err, html) {
+    if (err) {
+      callback(err)
+    } else {
+      callback(null, html)
+    }
+  })
+}
+
+/**
+ *
+ * @param outputPath
+ * @param streamFile
+ */
+function writeFileStream(outputPath: string, streamFile: Buffer) {
+  fs.writeFile(outputPath, streamFile, function (err) {
+    if (err) return console.log(err)
+    console.log('generate file successfully')
+  })
+}
+
+/**
+ *
+ * @param base64Data
+ * @param pathFile
+ */
+function writeFileFromBase64(base64Data: string, pathFile: string) {
+  const bufferData = Buffer.from(base64Data, 'base64')
+
+  if (fs.existsSync(path.resolve(pathFile))) {
+    console.log('file exist, location... ', pathFile)
+    return true
+  }
+
+  console.log('file not exist, creating file... ', pathFile)
+  fs.writeFileSync(pathFile, bufferData)
+  return false
+}
+
+export { readHTMLFile, writeFileFromBase64, writeFileStream }
