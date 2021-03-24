fe1f685214bff19ce7e2f1335332d1773e715a25
masb0ymas <fajriajjha@gmail.com>
2021-03-24 07:40:33 +0700

    feat(helper): add send mail helper

---
+++ src/helpers/SendEmail.ts
@@ -0,0 +1,42 @@
+import path from 'path'
+import handlebars from 'handlebars'
+import { readHTMLFile } from 'helpers/File'
+import EmailProvider from 'config/EmailProvider'
+import ResponseError from 'modules/Response/ResponseError'
+import { BASE_URL_CLIENT } from 'config/baseURL'
+import { EmailAttributes, UserAttributes } from 'models/user'
+
+const { APP_NAME } = process.env
+
+class SendMail {
+  /**
+   *
+   * @param formData
+   * @param token
+   */
+  public static AccountRegister(formData: UserAttributes, token: string) {
+    const { email, fullName }: EmailAttributes = formData
+    const pathTemplate = path.resolve(
+      __dirname,
+      `../../public/templates/emails/register.html`
+    )
+
+    const subject = 'Email Verification'
+    const urlToken = `${BASE_URL_CLIENT}/email/verify?token=${token}`
+    const dataTemplate = { APP_NAME, fullName, urlToken }
+    const Email = new EmailProvider()
+
+    readHTMLFile(pathTemplate, (error: Error, html: any) => {
+      if (error) {
+        throw new ResponseError.NotFound('email template not found')
+      }
+
+      const template = handlebars.compile(html)
+      const htmlToSend = template(dataTemplate)
+
+      Email.send(email, subject, htmlToSend)
+    })
+  }
+}
+
+export default SendMail
